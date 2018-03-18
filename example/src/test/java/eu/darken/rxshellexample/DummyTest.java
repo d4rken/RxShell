f99f2c09c9df743ef6113953e9baf0fc103ca399
Matthias Urhahn <darken@darken.eu>
2018-03-18 03:03:34 +0100

    Coveralls (#21)
    * Fixed coveralls (include all modules). *4
    
    * Fixed coveralls (include all modules). *5
    
    * Fixed coveralls (include all modules). *6
    
    * Fixed coveralls (include all modules). *7
    
    * Fixed coveralls (include all modules). *8
    
    * Fixed coveralls (include all modules). *9
    
    * Fixed coveralls (include all modules). *10
    
    * Fixed coveralls (include all modules). *10
    
    * Fixed coveralls (include all modules). *11

---
+++ example/src/test/java/eu/darken/rxshellexample/DummyTest.java
@@ -0,0 +1,12 @@
+package eu.darken.rxshellexample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class DummyTest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
